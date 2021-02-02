c9cf721bc309fdeeb650de9f2bc7cd895e16d70a
shulgao69 <shulgao69@mail.ru>
2021-02-02 15:09:47 +0300

    Second commit

--- Project5/__init__.py
+++ Project5/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 from Project5.config import Config
 from Project5.models import db
+migrate = Migrate()
 
 
 app = Flask(__name__)
@@ -9,6 +11,6 @@ app.config.from_object(Config)
 
 
 db.init_app(app)
-
+migrate.init_app(app, db)
 
 from Project5.views import *
